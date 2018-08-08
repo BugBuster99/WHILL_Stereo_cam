0e3cf9d75f1c66214d5a35d9c310ab168b51dc58
seiya0412 <shimizu@whill.jp>
2018-08-08 10:17:47 +0900

    Fix exposure runtime error

--- libuvc_camera/src/auto_exposure.py
+++ libuvc_camera/src/auto_exposure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# -*- codign: utf-8 -*-
-from math import log2
+# -*- coding: utf-8 -*-
+from math import log, pow, fabs
 import rospy
 from dynamic_reconfigure.client import Client
 from std_msgs.msg import String
@@ -13,11 +13,12 @@ import cv2
 
 
 class ev_calculator:
-    def __init__(self, camera_node_name='left_master', brightness_tgt=128):
+    def __init__(self, camera_node_name='left_master', brightness_tgt=128.0):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/src_image", Image, callback=self.img_callback)
+        self.image_sub = rospy.Subscriber("src_image", Image, callback=self.img_callback)
         self.client = Client(camera_node_name, timeout=30, config_callback=self.config_callback)
         self.__brightness_tgt = brightness_tgt
+        rospy.loginfo("dynparam server name = %s", camera_node_name)
 
     def img_callback(self, msg):
         try:
@@ -26,21 +27,35 @@ class ev_calculator:
             print(e)
 
 
-        brightness_pre = cv2.mean(cv_image)
+        sample = cv_image[240:240+480, 320:320+640]
+        brightness_pre = cv2.mean(sample)[0]
         params = self.client.get_configuration()
         old_exposure_absolute = params['exposure_absolute']
-        new_exposure_absolute = old_exposure_absolute + log2(brightness_pre) - log2(self.__brightness_tgt)
+        new_exposure_absolute = pow(2.0, log(self.__brightness_tgt, 2) - log(brightness_pre, 2) + log(old_exposure_absolute, 2))
+        if fabs(new_exposure_absolute - old_exposure_absolute) > 0.05:
+            if new_exposure_absolute < old_exposure_absolute:
+                new_exposure_absolute = old_exposure_absolute - 0.05
+            else:
+                new_exposure_absolute = old_exposure_absolute + 0.05
+        #new_exposure_absolute = old_exposure_absolute * self.__brightness_tgt / brightness_pre
+        rospy.loginfo("current brightness = %f", brightness_pre)
+        #rospy.loginfo("tgt BL = %f", log(brightness_tgt, 2))
+        #rospy.loginfo("log BL = %f", log(brightness_pre, 2))
+        rospy.loginfo("old EV = %f", old_exposure_absolute)
+        rospy.loginfo("new EV = %f", new_exposure_absolute)
+
         params['exposure_absolute'] = new_exposure_absolute
         self.client.update_configuration(params)
 
     def config_callback(self, config):
-        rospy.loginfo("Config set to {exposure_absolute}".format(**config))
+        rospy.loginfo("exposure_absolute set to {exposure_absolute}".format(**config))
     
 
 if __name__ == "__main__":
     rospy.init_node("auto_exposure_controller")
-    camera_node_name = rospy.get_param('camera_node_name', 'left_master')
-    brightness_tgt   = rospy.get_param('brightness_tgt', 128)
+    camera_node_name = rospy.get_param('~camera_node_name', 'left_master')
+    print camera_node_name
+    brightness_tgt   = rospy.get_param('~brightness_tgt', 128.0)
     calc = ev_calculator(camera_node_name, brightness_tgt)
     while not rospy.is_shutdown():
         rospy.spin()
