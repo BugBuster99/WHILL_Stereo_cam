fcf71eb49352e71b002aa0939f249c9b42f5a88f
seiya0412 <shimizu@whill.jp>
2018-08-08 11:00:32 +0900

    Add update interval

--- libuvc_camera/src/auto_exposure.py
+++ libuvc_camera/src/auto_exposure.py
@@ -13,42 +13,44 @@ import cv2
 
 
 class ev_calculator:
-    def __init__(self, camera_node_name='left_master', brightness_tgt=128.0, step_size=0.001):
+    def __init__(self, camera_node_name='left_master', brightness_tgt=128.0, step_size=0.001, update_interval=5):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("src_image", Image, callback=self.img_callback)
         self.client = Client(camera_node_name, timeout=30, config_callback=self.config_callback)
         self.__brightness_tgt = brightness_tgt
         self.__step_size = step_size
+        self.__update_interval = update_interval
         rospy.loginfo("dynparam server name = %s", camera_node_name)
 
     def img_callback(self, msg):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
-        except CvBridgeError as e:
-            print(e)
+        if msg.header.seq % self.__update_interval == 0:
+            try:
+                cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
+            except CvBridgeError as e:
+                print(e)
 
 
-        sample = cv_image[240:240+480, 320:320+640]
-        brightness_pre = cv2.mean(sample)[0]
-        params = self.client.get_configuration()
-        old_exposure_absolute = params['exposure_absolute']
-        new_exposure_absolute = pow(2.0, log(self.__brightness_tgt, 2) - log(brightness_pre, 2) + log(old_exposure_absolute, 2))
-        #new_exposure_absolute = old_exposure_absolute * self.__brightness_tgt / brightness_pre
-        if fabs(new_exposure_absolute - old_exposure_absolute) > self.__step_size:
-            if new_exposure_absolute < old_exposure_absolute:
-                new_exposure_absolute = old_exposure_absolute - self.__step_size 
-            else:
-                new_exposure_absolute = old_exposure_absolute + self.__step_size 
-        if new_exposure_absolute > 0.1:
-            new_exposure_absolute = 0.1
-        rospy.loginfo("current brightness = %f", brightness_pre)
-        #rospy.loginfo("tgt BL = %f", log(brightness_tgt, 2))
-        #rospy.loginfo("log BL = %f", log(brightness_pre, 2))
-        rospy.loginfo("old EV = %f", old_exposure_absolute)
-        rospy.loginfo("new EV = %f", new_exposure_absolute)
+            sample = cv_image[240:240+480, 320:320+640]
+            brightness_pre = cv2.mean(sample)[0]
+            params = self.client.get_configuration()
+            old_exposure_absolute = params['exposure_absolute']
+            new_exposure_absolute = pow(2.0, log(self.__brightness_tgt, 2) - log(brightness_pre, 2) + log(old_exposure_absolute, 2))
+            #new_exposure_absolute = old_exposure_absolute * self.__brightness_tgt / brightness_pre
+            if fabs(new_exposure_absolute - old_exposure_absolute) > self.__step_size:
+                if new_exposure_absolute < old_exposure_absolute:
+                    new_exposure_absolute = old_exposure_absolute - self.__step_size 
+                else:
+                    new_exposure_absolute = old_exposure_absolute + self.__step_size 
+            if new_exposure_absolute > 0.1:
+                new_exposure_absolute = 0.1
+            rospy.loginfo("current brightness = %f", brightness_pre)
+            #rospy.loginfo("tgt BL = %f", log(brightness_tgt, 2))
+            #rospy.loginfo("log BL = %f", log(brightness_pre, 2))
+            rospy.loginfo("old EV = %f", old_exposure_absolute)
+            rospy.loginfo("new EV = %f", new_exposure_absolute)
 
-        params['exposure_absolute'] = new_exposure_absolute
-        self.client.update_configuration(params)
+            params['exposure_absolute'] = new_exposure_absolute
+            self.client.update_configuration(params)
 
     def config_callback(self, config):
         rospy.loginfo("exposure_absolute set to {exposure_absolute}".format(**config))
